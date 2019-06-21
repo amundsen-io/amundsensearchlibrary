ffebbccecd97ec1ec14e4e558ad4b98a0937e513
Tao feng <fengtao04@gmail.com>
2019-06-20 22:35:04 -0700

    Fix ES proxy to handle additional attribute

--- search_service/proxy/elasticsearch.py
+++ search_service/proxy/elasticsearch.py
@@ -72,7 +72,13 @@ class ElasticsearchProxy(BaseProxy):
         for hit in response:
 
             try:
-                results.append(model(**vars(hit)))
+                LOGGING.info(hit)
+                result = {}
+                for attr in hit.__dict__:
+                    if attr in model.get_attrs():
+                        result[attr] = getattr(hit, attr)
+                LOGGING.info(result)
+                results.append(model(**result))
             except Exception:
                 LOGGING.exception('The record doesnt contain specified field.')
 
